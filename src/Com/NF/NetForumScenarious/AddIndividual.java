17560a2ff03b616fc8a58b51d5d705ea7cd78679
patildinesh19 <patildinesh19@hotmail.com>
2020-02-16 22:13:25 +0530

    update in new branch develop project

---
+++ src/Com/NF/NetForumScenarious/AddIndividual.java
@@ -0,0 +1,12 @@
+package Com.NF.NetForumScenarious;
+
+import org.testng.annotations.Test;
+
+public class AddIndividual 
+{
+	@Test
+	public void addindtoorg()
+	{
+		System.out.println("Add in to org sucessfully");
+	}
+}
